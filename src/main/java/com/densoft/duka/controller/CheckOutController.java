1ecbcce7550250d885c6e35429ab66fce1793673
dennis <wakahiad@gmail.com>
2022-05-20 11:44:25 +0300

    stripe payment setup

--- src/main/java/com/densoft/duka/controller/CheckOutController.java
+++ src/main/java/com/densoft/duka/controller/CheckOutController.java
@@ -1,9 +1,14 @@
 package com.densoft.duka.controller;
 
+import com.densoft.duka.dto.PaymentInfo;
 import com.densoft.duka.dto.Purchase;
 import com.densoft.duka.dto.PurchaseResponse;
 import com.densoft.duka.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,5 +23,12 @@ public class CheckOutController {
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/densoft/duka/dto/PaymentInfo.java
@@ -0,0 +1,9 @@
+package com.densoft.duka.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+    private int amount;
+    private String currency;
+}
